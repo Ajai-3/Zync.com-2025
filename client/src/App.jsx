b46d1d974d7af7c9a9b82ad4d1718b7e56c3e627
Ajai-3 <ajaipjayan@gmail.com>
2025-02-21 10:20:35 +0530

    pages created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { lazy } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+
+const Home = lazy(() => import("./pages/Home"))
+const Login = lazy(() => import("./pages/Login"))
+const Chat = lazy(() => import("./pages/Chat"))
+const Groups = lazy(() => import("./pages/Groups"))
 
 const App = () => {
   return (
-    <div>App</div>
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element = { <Home/> } />
+      <Route path='/Login' element = { <Login /> } />
+      <Route path='/Chat' element = { <Chat /> } />
+      <Route path='/Groups' element = { <Groups /> } />
+      <Route path='/About' element = { <h1>About</h1> } />
+     </Routes>
+     </BrowserRouter>
   )
 }
 
